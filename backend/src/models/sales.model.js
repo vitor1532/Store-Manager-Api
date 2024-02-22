80d1bd156f437c0e6fc56598e754747464911fa8
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-02-22 20:00:44 -0300

    adaptando queries SQL

--- backend/src/models/sales.model.js
+++ backend/src/models/sales.model.js
@@ -1,18 +1,23 @@
+const camelize = require('camelize');
 const connection = require('./connection');
 
 const findAll = async () => {
-  const [sales] = await connection.execute('SELECT * FROM sales');
-
-  return sales;
+  const [sales] = await connection.execute(
+    `SELECT sp.sale_id, sa.date, sp.product_id, sp.quantity FROM sales_products as sp
+      JOIN sales as sa ON sp.sale_id = sa.id`,
+  );
+  return camelize(sales);
 };
 
 const findById = async (id) => {
   const [[sale]] = await connection.execute(
-    'SELECT * FROM sales WHERE id = ?',
+    `SELECT sp.sale_id, sa.date, sp.product_id, sp.quantity FROM sales_products as sp
+      JOIN sales as sa ON sp.sale_id = sa.id
+      WHERE sp.sale_id = ?`,
     [id],
   );
 
-  return sale;
+  return camelize(sale);
 };
 
 module.exports = {
