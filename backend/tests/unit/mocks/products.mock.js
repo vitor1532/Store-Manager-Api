80ad9d179b8f4da5ccb26e5aa6cbbd3654785412
vitor1532 <vitomartinsalmeida@gmail.com>
2024-02-22 16:44:24 -0300

    mockando respostas da camada service

--- backend/tests/unit/mocks/products.mock.js
+++ backend/tests/unit/mocks/products.mock.js
@@ -10,7 +10,30 @@ const productsFromModel = [
   { id: 3, name: 'Espada de Tyr' },
 ];
 
+const successfulResponseFindAllFromService = {
+  status: 'SUCCESSFUL',
+  data: productsFromModel,
+};
+
+const errorResponseFindAllFromService = {
+  status: 'NOT_FOUND', data: { message: 'Products not found' },
+};
+
+const successfulResponseFindByIdFromService = {
+  status: 'SUCCESSFUL',
+  data: productsFromModel[0],
+};
+
+const errorResponseFindByIdFromService = {
+  status: 'NOT_FOUND',
+  data: { message: 'Product not found' },
+};
+
 module.exports = {
   productsFromDB,
   productsFromModel,
+  successfulResponseFindAllFromService,
+  errorResponseFindAllFromService,
+  successfulResponseFindByIdFromService,
+  errorResponseFindByIdFromService,
 };
--- backend/tests/unit/services/products.service.test.js
+++ backend/tests/unit/services/products.service.test.js
@@ -4,66 +4,53 @@ const connection = require('../../../src/models/connection');
 const { productsService } = require('../../../src/services');
 const { 
   productsFromDB,
-  productsFromModel,
+  successfulResponseFindAllFromService,
+  errorResponseFindAllFromService,
+  successfulResponseFindByIdFromService,
+  errorResponseFindByIdFromService,
 } = require('../mocks/products.mock');
 
 describe('Testa o service de Products', function () {
   it('Testa se a função retorna todos os produtos do banco', async function () {
     // arrange
     sinon.stub(connection, 'execute').resolves([productsFromDB]);
-    const responseFromService = {
-      status: 'SUCCESSFUL',
-      data: productsFromModel,
-    };
     // act
     const products = await productsService.findAll();
     // assert
     expect(products).to.be.an('object');
-    expect(products).to.be.deep.equal(responseFromService);
+    expect(products).to.be.deep.equal(successfulResponseFindAllFromService);
   });
   
   it('Retorna a mensagem de erro caso não exista dados no banco', async function () {
     // arrange
     sinon.stub(connection, 'execute').resolves([false]);
-    const failResponse = {
-      status: 'NOT_FOUND', data: { message: 'Products not found' },
-    };
     // act
     const products = await productsService.findAll();
-
     // assert
     expect(products).to.be.an('object');
-    expect(products).to.be.deep.equal(failResponse);
+    expect(products).to.be.deep.equal(errorResponseFindAllFromService);
   });
 
   it('Testa se a função retorna um produto do banco', async function () {
     // arrange
     sinon.stub(connection, 'execute').resolves([[productsFromDB[0]]]);
     const id = 1;
-    const responseFromService = {
-      status: 'SUCCESSFUL',
-      data: productsFromModel[0],
-    };
     // act
     const products = await productsService.findById(id);
     // assert
     expect(products).to.be.an('object');
-    expect(products).to.be.deep.equal(responseFromService);
+    expect(products).to.be.deep.equal(successfulResponseFindByIdFromService);
   });
 
   it('Testa se a função retorna uma mensagem de erro caso o produto não exista', async function () {
     // arrange
     sinon.stub(connection, 'execute').resolves([[false]]);
     const id = 9999;
-    const responseFromService = {
-      status: 'NOT_FOUND',
-      data: { message: 'Product not found' },
-    };
     // act
     const products = await productsService.findById(id);
     // assert
     expect(products).to.be.an('object');
-    expect(products).to.be.deep.equal(responseFromService);
+    expect(products).to.be.deep.equal(errorResponseFindByIdFromService);
   });
   afterEach(function () {
     sinon.restore();
