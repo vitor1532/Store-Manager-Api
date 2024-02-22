1304def64f7ff490bb98029148bc6f259c5680a3
vitor1532 <vitomartinsalmeida@gmail.com>
2024-02-22 16:21:27 -0300

    testando a camada service

--- backend/tests/unit/services/products.service.test.js
+++ backend/tests/unit/services/products.service.test.js
@@ -18,6 +18,21 @@ describe('Testa o service de Products', function () {
     expect(products).to.be.an('object');
     expect(products).to.be.deep.equal(responseFromService);
   });
+
+  it('Testa se a função retorna um produto do banco', async function () {
+    // arrange
+    sinon.stub(connection, 'execute').resolves([[productsFromDB[0]]]);
+    const id = 1;
+    const responseFromService = {
+      status: 'SUCCESSFUL',
+      data: productsFromModel[0],
+    };
+    // act
+    const products = await productsService.findById(id);
+    // assert
+    expect(products).to.be.an('object');
+    expect(products).to.be.deep.equal(responseFromService);
+  });
   afterEach(function () {
     sinon.restore();
   });
