8a124cc43a12fd8c35fa66f8c64197377a6744b2
vitor1532 <vitomartinsalmeida@gmail.com>
2024-02-22 16:38:28 -0300

    testando casos de erro na camada productService

--- backend/tests/unit/services/products.service.test.js
+++ backend/tests/unit/services/products.service.test.js
@@ -2,7 +2,10 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const connection = require('../../../src/models/connection');
 const { productsService } = require('../../../src/services');
-const { productsFromDB, productsFromModel } = require('../mocks/products.mock');
+const { 
+  productsFromDB,
+  productsFromModel,
+} = require('../mocks/products.mock');
 
 describe('Testa o service de Products', function () {
   it('Testa se a função retorna todos os produtos do banco', async function () {
@@ -18,6 +21,20 @@ describe('Testa o service de Products', function () {
     expect(products).to.be.an('object');
     expect(products).to.be.deep.equal(responseFromService);
   });
+  
+  it('Retorna a mensagem de erro caso não exista dados no banco', async function () {
+    // arrange
+    sinon.stub(connection, 'execute').resolves([false]);
+    const failResponse = {
+      status: 'NOT_FOUND', data: { message: 'Products not found' },
+    };
+    // act
+    const products = await productsService.findAll();
+
+    // assert
+    expect(products).to.be.an('object');
+    expect(products).to.be.deep.equal(failResponse);
+  });
 
   it('Testa se a função retorna um produto do banco', async function () {
     // arrange
@@ -26,6 +43,21 @@ describe('Testa o service de Products', function () {
     const responseFromService = {
       status: 'SUCCESSFUL',
       data: productsFromModel[0],
+    };
+    // act
+    const products = await productsService.findById(id);
+    // assert
+    expect(products).to.be.an('object');
+    expect(products).to.be.deep.equal(responseFromService);
+  });
+
+  it('Testa se a função retorna uma mensagem de erro caso o produto não exista', async function () {
+    // arrange
+    sinon.stub(connection, 'execute').resolves([[false]]);
+    const id = 9999;
+    const responseFromService = {
+      status: 'NOT_FOUND',
+      data: { message: 'Product not found' },
     };
     // act
     const products = await productsService.findById(id);
