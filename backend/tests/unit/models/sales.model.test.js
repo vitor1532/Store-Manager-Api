273e7173a058974b7bf9c7d4bc846a63ab2c94f2
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-02-22 21:23:03 -0300

    testando a camada salesModel

---
+++ backend/tests/unit/models/sales.model.test.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const { salesModel } = require('../../../src/models');
+const { salesFromDB, salesFromModel } = require('../mocks/sales.mock');
+
+describe('Testa o model de Sales', function () {
+  it('Recupera todos as sales com sucesso', async function () {
+    // arrange
+    sinon.stub(connection, 'execute').resolves([salesFromDB]);
+    // act
+    const products = await salesModel.findAll();
+    // assert
+    expect(products).to.be.an('array');
+    expect(products).to.have.lengthOf(3);
+    expect(products).to.be.deep.equal(salesFromModel);
+  });
+
+  it('Recupera apenas uma das sales com sucesso', async function () {
+    // arrange
+    const id = 1;
+    sinon.stub(connection, 'execute').resolves([salesFromDB[0]]);
+    // act
+    const product = await salesModel.findById(id);
+    // assert
+    expect(product).to.be.an('object');
+    expect(product).to.be.deep.equal(salesFromModel[0]);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
