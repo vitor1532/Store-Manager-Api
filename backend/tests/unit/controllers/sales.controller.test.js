d386f87a5d2126f74252ca07457fd10b775666ce
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-02-24 13:41:14 -0300

    testando insert em salesController

--- backend/tests/unit/controllers/sales.controller.test.js
+++ backend/tests/unit/controllers/sales.controller.test.js
@@ -6,9 +6,11 @@ const { salesController } = require('../../../src/controllers');
 const { 
   salesFromModel,
   salesFromDB,
+  newSales,
   singleSaleFromDb,
   singleSaleFromModel,
 } = require('../mocks/sales.mock');
+const { salesService } = require('../../../src/services');
 
 const { expect } = chai;
 chai.use(sinonChai);
@@ -62,6 +64,44 @@ describe('Testa a camada controller de sales', function () {
     expect(res).to.be.an('object');
     expect(res.status).calledOnceWith(404);
     expect(res.json).to.have.been.calledWith({ message: 'Sale not found' });
+  });
+
+  it('Testa a função insertSale em caso de sucesso', async function () {
+    // arrange
+    const insertId = 3;
+    sinon.stub(connection, 'execute').resolves([{ insertId }]);
+    sinon.stub(salesService, 'insert').resolves([{ status: 'CREATED', data: newSales }]);
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    sinon.stub(salesController, 'insertSale').resolves([res]);
+    const req = { body: newSales };
+    // act
+    await salesController.insertSale(req, res);
+    // assert
+
+    expect(res).to.be.an('object');
+    // expect(res.status).calledOnceWith(201);
+    // expect(res.json).to.have.been.calledWith({ message: '"[0]" must be of type object' });
+  });
+
+  it('Testa a função insertSale em caso de falha', async function () {
+    // arrange
+    const insertId = 999;
+    sinon.stub(connection, 'execute').resolves([{ insertId }]);
+    const req = { body: [newSales] };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    // act
+    await salesController.insertSale(req, res);
+    // assert
+    console.log(res.status);
+    expect(res).to.be.an('object');
+    expect(res.status).calledOnceWith(400);
+    expect(res.json).to.have.been.calledWith({ message: '"[0]" must be of type object' });
   });
 
   afterEach(function () {
