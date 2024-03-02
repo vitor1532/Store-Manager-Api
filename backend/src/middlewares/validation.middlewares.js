dd12f03c8a8e12ec906e15d3bacda083f257846c
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-02 15:39:01 -0300

    implementando middleware de erros nos middlewares de validação

--- backend/src/middlewares/validation.middlewares.js
+++ backend/src/middlewares/validation.middlewares.js
@@ -2,18 +2,22 @@ const { productsModel } = require('../models');
 const mapStatusHTTP = require('../utils/mapStatusHTTP');
 
 const validateInputNewSale = async (sales, next) => {
-  const error = [];
-  const promises = sales.map(async (sale) => {
-    const productExists = sale.productId ? await productsModel.findById(sale.productId) : next();
-    if (!productExists) {
-      error.push({ 
-        status: 'NOT_FOUND', data: { message: 'Product not found' } }); 
-    }
-  });
+  try {
+    const error = [];
+    const promises = sales.map(async (sale) => {
+      const productExists = sale.productId ? await productsModel.findById(sale.productId) : next();
+      if (!productExists) {
+        error.push({ 
+          status: 'NOT_FOUND', data: { message: 'Product not found' } }); 
+      }
+    });
 
-  await Promise.all(promises);
+    await Promise.all(promises);
 
-  return error;
+    return error; 
+  } catch (error) {
+    return next(error);
+  }
 };
 
 const validateProductsExistsMiddleware = async (req, res, next) => {
@@ -24,9 +28,9 @@ const validateProductsExistsMiddleware = async (req, res, next) => {
 
     if (error.length > 0) return res.status(mapStatusHTTP(error[0].status)).json(error[0].data);
     next();
-  } catch (err) {
-    console.log(err);
-  } 
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = {
