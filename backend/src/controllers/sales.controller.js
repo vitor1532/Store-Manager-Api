2dab1ad09ae173df72c7e61bcc1bcedc3c96dfbb
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-02 15:33:17 -0300

    aplicando middleware de tratamento de erro em sales.controller

--- backend/src/controllers/sales.controller.js
+++ backend/src/controllers/sales.controller.js
@@ -1,41 +1,61 @@
 const { salesService } = require('../services');
 const mapStatusHTTP = require('../utils/mapStatusHTTP');
 
-const getAllSales = async (_req, res) => {
-  const { status, data } = await salesService.findAll();
+const getAllSales = async (_req, res, next) => {
+  try {
+    const { status, data } = await salesService.findAll();
 
-  return res.status(mapStatusHTTP(status)).json(data);
+    return res.status(mapStatusHTTP(status)).json(data); 
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const getSaleById = async (req, res) => {
-  const { id } = req.params;
-  const { status, data } = await salesService.findById(id);
+const getSaleById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { status, data } = await salesService.findById(id);
 
-  return res.status(mapStatusHTTP(status)).json(data);
+    return res.status(mapStatusHTTP(status)).json(data); 
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const insertSale = async (req, res) => {
-  const { status, data } = await salesService.insert(req.body);
-  return res.status(mapStatusHTTP(status)).json(data);
+const insertSale = async (req, res, next) => {
+  try {
+    const { status, data } = await salesService.insert(req.body);
+    return res.status(mapStatusHTTP(status)).json(data); 
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const deleteSale = async (req, res) => {
-  const { id } = req.params;
-  const { status, data } = await salesService.remove(id);
+const deleteSale = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { status, data } = await salesService.remove(id);
 
-  return res.status(mapStatusHTTP(status)).json(data);
+    return res.status(mapStatusHTTP(status)).json(data); 
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const updateProductQuantity = async (req, res) => {
-  const { saleId, productId } = req.params;
+const updateProductQuantity = async (req, res, next) => {
+  try {
+    const { saleId, productId } = req.params;
 
-  const { status, data } = await salesService.updateProductQuantity(
-    saleId,
-    productId,
-    req.body,
-  );
+    const { status, data } = await salesService.updateProductQuantity(
+      saleId,
+      productId,
+      req.body,
+    );
 
-  return res.status(mapStatusHTTP(status)).json(data);
+    return res.status(mapStatusHTTP(status)).json(data); 
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = {
