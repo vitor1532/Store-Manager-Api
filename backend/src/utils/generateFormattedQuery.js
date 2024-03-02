061c7a2bb888711e77359c0891eb60977e112ffa
Vitor Martins <vitomartinsalmeida@gmail.com>
2024-03-02 15:43:00 -0300

    removendo comentários

--- backend/src/utils/generateFormattedQuery.js
+++ backend/src/utils/generateFormattedQuery.js
@@ -8,21 +8,6 @@ const getFormattedUpdateColumns = (object) => Object.keys(snakeize(object))
   .map((key) => `${key} = ?`)
   .join(', ');
 
-// console.log('columns: ', getFormattedColumnNames({
-//   productId: 2,
-//   quantity: 5,
-// }));
-
-// console.log('placeholders: ', `${getFormattedPlaceholders({
-//   productId: 2,
-//   quantity: 5,
-// })}`);
-
-// console.log('formattedString: ', `${getFormattedUpdateColumns({
-//   productId: 2,
-//   quantity: 5,
-// })}`);
-
 module.exports = {
   getFormattedColumnNames,
   getFormattedPlaceholders,
